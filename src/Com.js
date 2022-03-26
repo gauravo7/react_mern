dddd5d1c6d8d72f835260e541a8615534af66055
gauravo7 <gaurav@o7services.com>
2022-03-26 11:48:08 +0530

    intial

---
+++ src/Com.js
@@ -0,0 +1,11 @@
+import './com.css'; 
+function Com(){
+    return(
+        <div>
+            <h2>Welcome to Com Component</h2>
+            <h3>Welcome to Com H3 Component</h3>
+        </div>
+    );
+}
+
+export default Com;
